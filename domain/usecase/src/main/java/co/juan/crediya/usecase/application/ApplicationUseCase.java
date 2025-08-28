c25007fd409252a5ec66bae3f930fd00595a15af
Juan Ceballos <juandavi789@hotmail.com>
2025-08-28 11:11:40 -0500

    Added logic to the useCase so we can check if the user exist and get his email and check if the loan type exist

--- domain/usecase/src/main/java/co/juan/crediya/usecase/application/ApplicationUseCase.java
+++ domain/usecase/src/main/java/co/juan/crediya/usecase/application/ApplicationUseCase.java
@@ -2,8 +2,10 @@ package co.juan.crediya.usecase.application;
 
 import co.juan.crediya.model.application.Application;
 import co.juan.crediya.model.application.gateways.ApplicationRepository;
+import co.juan.crediya.model.dto.LoanApplicationDTO;
 import co.juan.crediya.model.exceptions.CrediYaException;
 import co.juan.crediya.model.exceptions.ErrorCode;
+import co.juan.crediya.model.user.UserGateway;
 import co.juan.crediya.usecase.loantype.LoanTypeUseCase;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
@@ -14,14 +16,36 @@ public class ApplicationUseCase {
 
     private final ApplicationRepository applicationRepository;
     private final LoanTypeUseCase loanTypeUseCase;
+    private final UserGateway userGateway;
 
-    public Mono<Application> saveApplication(Application application) {
-        return loanTypeUseCase.getLoanTypeById(application.getIdLoanType())
-                .switchIfEmpty(Mono.error(new CrediYaException(ErrorCode.INVALID_LOAN_TYPE)))
-                .flatMap(loanType -> {
-                    application.setIdState(1L);
+    public Mono<Application> saveApplication(LoanApplicationDTO loanApplicationDTO) {
+
+        Mono<String> userEmailByDniMono = userGateway.getUserEmailByDni(loanApplicationDTO.getDni());
+        Mono<Boolean> loanTypeMono = loanTypeUseCase.getLoanTypeById(loanApplicationDTO.getIdLoanType()).hasElement();
+
+        return Mono.zip(userEmailByDniMono, loanTypeMono)
+                .flatMap(result -> {
+                    String userEmail = result.getT1();
+                    boolean loanType = result.getT2();
+
+                    if (userEmail.isBlank()) {
+                        return Mono.error(new CrediYaException(ErrorCode.USER_NOT_FOUND));
+                    }
+
+                    if (!loanType) {
+                        return Mono.error(new CrediYaException(ErrorCode.INVALID_LOAN_TYPE));
+                    }
+
+                    Application application = Application.builder()
+                            .email(userEmail)
+                            .term(loanApplicationDTO.getTerm())
+                            .amount(loanApplicationDTO.getAmount())
+                            .idLoanType(loanApplicationDTO.getIdLoanType())
+                            .idState(1L).build();
+
                     return applicationRepository.saveApplication(application);
                 });
+
     }
 
     public Flux<Application> getAllApplications() {
