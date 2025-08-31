eb5ed06879aa00ed7d04ee4c910421f6b8823d9a
Juan Ceballos <juandavi789@hotmail.com>
2025-08-31 16:50:29 -0500

    Remove unnecessary import

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/juan/crediya/consumer/config/RestConsumerConfig.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/juan/crediya/consumer/config/RestConsumerConfig.java
@@ -3,7 +3,6 @@ package co.juan.crediya.consumer.config;
 import co.juan.crediya.security.SecurityContextRepository;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
