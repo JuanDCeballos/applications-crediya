52cc939255cc660fc4698d3ab5379b726936789e
Juan Ceballos <juandavi789@hotmail.com>
2025-08-31 16:49:51 -0500

    Adjust the way we send the JWT Token for other services

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/juan/crediya/consumer/config/RestConsumerConfig.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/juan/crediya/consumer/config/RestConsumerConfig.java
@@ -1,14 +1,19 @@
 package co.juan.crediya.consumer.config;
 
+import co.juan.crediya.security.SecurityContextRepository;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import static io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS;
@@ -17,12 +22,15 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 @Configuration
 public class RestConsumerConfig {
 
+    private final SecurityContextRepository securityContextRepository;
+
     private final String url;
 
     private final int timeout;
 
-    public RestConsumerConfig(@Value("${adapter.restconsumer.url}") String url,
+    public RestConsumerConfig(SecurityContextRepository securityContextRepository, @Value("${adapter.restconsumer.url}") String url,
                               @Value("${adapter.restconsumer.timeout}") int timeout) {
+        this.securityContextRepository = securityContextRepository;
         this.url = url;
         this.timeout = timeout;
     }
@@ -30,16 +38,14 @@ public class RestConsumerConfig {
     @Bean
     public WebClient getWebClient(WebClient.Builder builder) {
         return builder
-            .baseUrl(url)
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-            .clientConnector(getClientHttpConnector())
-            .build();
+                .baseUrl(url)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .clientConnector(getClientHttpConnector())
+                .filter(addJwtTokenToHeader())
+                .build();
     }
 
     private ClientHttpConnector getClientHttpConnector() {
-        /*
-        IF YO REQUIRE APPEND SSL CERTIFICATE SELF SIGNED: this should be in the default cacerts trustore
-        */
         return new ReactorClientHttpConnector(HttpClient.create()
                 .compress(true)
                 .keepAlive(true)
@@ -48,6 +54,13 @@ public class RestConsumerConfig {
                     connection.addHandlerLast(new ReadTimeoutHandler(timeout, MILLISECONDS));
                     connection.addHandlerLast(new WriteTimeoutHandler(timeout, MILLISECONDS));
                 }));
+    }
+
+    private ExchangeFilterFunction addJwtTokenToHeader() {
+        return ExchangeFilterFunction.ofRequestProcessor(request -> Mono.just(ClientRequest.from(request)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + securityContextRepository.getToken())
+                        .build())
+                .defaultIfEmpty(request));
     }
 
 }
