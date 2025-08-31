5b2c901b215cdee1c34f05e8380cb5b38bbdb5b3
Juan Ceballos <juandavi789@hotmail.com>
2025-08-31 16:52:03 -0500

    Added custom messages

--- domain/model/src/main/java/co/juan/crediya/model/exceptions/ErrorCode.java
+++ domain/model/src/main/java/co/juan/crediya/model/exceptions/ErrorCode.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 public enum ErrorCode {
     INVALID_LOAN_TYPE("BEC_NA", "There's not an application with that id", 409),
     DATABASE_ERROR("BEC_DBE", "An error has occurred while communicating with the database.", 500),
-    USER_NOT_FOUND("BEC_NFU", "The user with dni doesn't exists.", 404);
+    USER_NOT_FOUND("BEC_NFU", "The user with dni doesn't exists.", 404),
+    BAD_TOKEN("BEC_BT", "No token has been sent in the request.", 403),
+    INVALID_TOKEN("BEC_IT", "Invalid authentication in the request.", 403),
+    USER_NOT_MATCH("BEC_UNM", "Can't do a loan application for other user", 409);
 
     private final String businessErrorCode;
     private final String message;
