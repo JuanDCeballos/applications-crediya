848b6b5ea98165a1ebd3d6f2db14c6f4153b45d7
Juan Ceballos <juandavi789@hotmail.com>
2025-09-07 00:36:26 -0500

    Enable security to the app

--- infrastructure/driven-adapters/security/src/main/java/co/juan/crediya/security/SecurityConfig.java
+++ infrastructure/driven-adapters/security/src/main/java/co/juan/crediya/security/SecurityConfig.java
@@ -3,11 +3,15 @@ package co.juan.crediya.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationManager jwtAuthenticationManager;
